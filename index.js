e75c7e22676f211af7b13cba44f8bc768ae1cb91
CORBIsr <103318344+CORBIsr@users.noreply.github.com>
2022-11-25 16:36:06 +0100

    piccola modifica

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
 const Discord = require("discord.js")
 const client = new Discord.Client(
-    {intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
+    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
 
 client.login(process.env.token)
 
-client.on("ready", () =>{
+client.on("ready", () => {
     console.log("BOT ONLINE")
 })
-client.on("messageCreate", (message) =>{
-    if(message.author.username == "pancio"){
+client.on("messageCreate", (message) => {
+    if (message.author.username == "pancio") {
         message.channel.send("Il Bot è OFLINE :(")
-    }
-    if(message.content == "!help"){
-        var mes = new Discord.MessageEmbed()
-            .setTitle("Il Bot del Dio Mulo!")
-            .setDescription("!diomulo (descrizione)\n!ziomulo (Rica bestemia)")
-        message.channel.send({embeds: [mes]})
+    } else {
+        if (message.content == "!help") {
+            var mes = new Discord.MessageEmbed()
+                .setTitle("Il Bot del Dio Mulo!")
+                .setDescription("!diomulo (descrizione)\n!ziomulo (Rica bestemia)")
+            message.channel.send({ embeds: [mes] })
+        }
+
+        if (message.content == "!diomulo") {
+            var mes = new Discord.MessageEmbed()
+                .setTitle("Dio Mulo!")
+                .setDescription("https://m.facebook.com/Lagazzettadelmulo/?_rdr")
+            message.channel.send({ embeds: [mes] })
+        }
+        if (message.content == "!ziomulo") {
+            message.channel.send(`${message.author.username} ha detto: Dio Mulo`)
+        }
     }
 
-    if(message.content == "!diomulo"){
-        var mes = new Discord.MessageEmbed()
-            .setTitle("Dio Mulo!")
-            .setDescription("https://m.facebook.com/Lagazzettadelmulo/?_rdr")
-        message.channel.send({embeds: [mes]})
-    }
-    if(message.content == "!ziomulo"){
-        message.channel.send(`${message.author.username} ha detto: Dio Mulo`)
-    }
 })
