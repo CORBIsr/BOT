ed93c7d01a492c7ae6458321ea782a29b85778a1
CORBIsr <103318344+CORBIsr@users.noreply.github.com>
2022-11-26 19:01:13 +0100

    debug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const client = new Discord.Client(
     { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
 )
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 const prefisso = "!"
 
 client.login(process.env.token)
