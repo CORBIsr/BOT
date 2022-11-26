074999c74d7ec4c09f654c0ae629ccd16dd9ffe0
CORBIsr <103318344+CORBIsr@users.noreply.github.com>
2022-11-26 19:38:52 +0100

    rimozione select menu
    non andava per la seconda volta :(

--- index.js
+++ index.js
@@ -44,29 +44,6 @@ client.on("messageCreate", (message) => {
             message.channel.send("Ciao brodi!!")
         }
 
-        if (message.content === "ping") {
-            const row = new MessageActionRow()
-                .addComponents(
-                    new MessageSelectMenu()
-                        .setCustomId('select')
-                        .setPlaceholder('Nothing selected')
-                        .addOptions([
-                            {
-                                label: 'Select me',
-                                description: 'This is a description',
-                                value: 'first_option',
-                            },
-                            {
-                                label: 'You can select me too',
-                                description: 'This is also a description',
-                                value: 'second_option',
-                            },
-                        ]),
-                );
-    
-            message.channel.send({ content: 'Pong!', components: [row] });
-        }
-
 
     }
 
